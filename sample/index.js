8919c9747d431d774b77f85c1c40c7bde8cf3678
Ralf Kistner <ralf@journeyapps.com>
2017-12-01 16:58:54 -0700

    Add a sample.

---
+++ sample/index.js
@@ -0,0 +1,36 @@
+
+const pdf = require("@journeyapps/pdf-reports");
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_KEY);
+pdf.setApiToken(process.env.JOURNEY_PDF_KEY);
+const emailAddress = process.env.TEST_EMAIL;
+
+async function test() {
+    const [pdf1, pdf2] = [
+      await pdf.generatePdf({ url: 'https://journeyapps.com' }),
+      await pdf.generatePdf({ html: 'Plain html pdf' })
+    ];
+
+    const email = {
+        to: emailAddress,
+        from: emailAddress,
+        subject: 'Test email',
+        text: 'Test email',
+        attachments: [
+            await pdf1.toEmailAttachment('pdf1.pdf'),
+            await pdf2.toEmailAttachment('pdf2.pdf')
+        ]
+    };
+    try {
+      await sgMail.send(email);
+    } catch(error) {
+      // This helps for debugging
+      if (error.response) {
+        console.error(error.response.body);
+      }
+      throw error;
+    }
+}
+
+test().catch(console.error);
